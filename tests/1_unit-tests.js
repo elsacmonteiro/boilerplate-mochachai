2098628ce405e04471a0d00f3b9e5625f1fcd40b
Elsa Monteiro <elsa.cg.monteiro@gmail.com>
2025-01-29 13:11:24 +0000

    test 2

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -12,7 +12,7 @@ suite('Unit Tests', function () {
     test('#isDefined, #isUndefined', function () {
       assert.isDefined(null, 'null is not undefined');
       assert.isundefined(undefined, 'undefined IS undefined');
-      assert.isundefined('hello', 'A string is not undefined');
+      assert.isDefined('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
